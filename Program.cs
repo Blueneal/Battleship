7e0541bf456b2cab1f954531869781f0169a849e
Vinny Neal <vinmanneal@gmail.com>
2025-03-13 20:34:35 -0500

    Started player prompt for sleceting game mode, and started adding the grid

--- Program.cs
+++ Program.cs
@@ -1,11 +1,52 @@
-﻿namespace Battleship
+﻿using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+namespace Battleship
 {
     internal class Program
     {
-        public bool isPlaying;
+
         static void Main(string[] args)
         {
-            Title.TitleScreen();
+            bool isPlaying = false;
+
+            TitleScreen();
+            Console.WriteLine("Press Enter Key to Continue...");
+            Console.ReadLine();
+            isPlaying = true;
+            while (isPlaying == true)
+            {
+                Console.WriteLine("[1]One Player");
+                Console.WriteLine("[2]Two Players");
+                Console.WriteLine("[3]Quit");
+                string? input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    Console.WriteLine("Player vs AI Selected");
+                    Grid.PrintGrid();
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("Multiplayer Selected");
+                }
+                else if (input == "3")
+                {
+                    isPlaying = false;
+                    Console.WriteLine("Thank You For Playing!");
+                }
+            }
+        }
+
+        public static void TitleScreen()
+        {
+            Console.WriteLine(" ____        _   _   _           _     _");
+            Console.WriteLine("|  _ \\      | | | | | |         | |   (_)");
+            Console.WriteLine("| |_) | __ _| |_| |_| | ___  ___| |__  _ _ __");
+            Console.WriteLine("|  _ < / _' |  _|  _| |/ _ \\/ __| '_ \\| | '_ \\");
+            Console.WriteLine("| |_) | (_| | |_| |_| |  __/\\__ \\ | | | | |_) |");
+            Console.WriteLine("|____/ \\__._|__/|__/|_|\\___||___/_| |_|_| .__/");
+            Console.WriteLine("                                        | |");
+            Console.WriteLine("                                        |_|");
         }
     }
 }
